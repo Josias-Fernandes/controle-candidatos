70843d3a7a60bcff9e49eacfa2ca6c1861c4a23c
Josias-Fernandes <josiasfernands.work@outlook.com>
2023-09-21 00:11:11 -0300

    analisar candidatos adicionado cases

--- src/ProcessoSeletivo.java
+++ src/ProcessoSeletivo.java
@@ -1,6 +1,21 @@
 
 public class ProcessoSeletivo {
    public static void main(String[] args) {
-    System.out.println("Processo Seletivo 2023");
+    analisarCandidato(2300.00);
+    //analisarCandidato(2700.00);
+   // analisarCandidato(2500.00);
+
+   }
+   static void analisarCandidato(double salarioPretendido ){
+     double salarioBase = 2500.00;
+     if(salarioBase > salarioPretendido){
+       System.out.println("LIGAR para o CANDIDATO");
+     }else if(salarioBase == salarioPretendido){
+        System.out.println("Ligar para o candidato com CONTRA PROPOSTA");
+     } else {
+        System.out.println("AGUARDAR os demais CANDIDATOS");
+     }
+
+
    }
 }
